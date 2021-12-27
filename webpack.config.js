a3304a104628fb8a987eacf147af116888a43733
Vivek Nair <vivek@nair.me>
2021-12-26 21:17:22 -0800

    revamp build process

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname),
+        publicPath: '/',
+        filename: 'mfkdf.js'
+    },
+    module: {
+        noParse: /\.wasm$/,
+        rules: [
+            {
+                test: /\.wasm$/,
+                loader: 'base64-loader',
+                type: 'javascript/auto',
+            },
+        ],
+    },
+    resolve: {
+        fallback: {
+            path: false,
+            fs: false,
+            Buffer: false,
+            process: false,
+            crypto: require.resolve("crypto-browserify"),
+            buffer: require.resolve("buffer/"),
+            stream: require.resolve("stream-browserify")
+        },
+    },
+};
