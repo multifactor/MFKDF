25b04159a95921fb5e69d85d54d0d177dfbe2908
Vivek Nair <vivek@nair.me>
2022-01-10 10:56:23 -0800

    link docs to home

--- site/docs.js
+++ site/docs.js
@@ -1,1 +1,1 @@
-document.body.innerHTML += '<header id="htop"><div class="io"><img src="../logo.png"></div></header>';
+document.body.innerHTML += '<header id="htop"><div class="io"><a href="https://mfkdf.com"><img src="../logo.png"></a></div></header>';
