480506ec33b4773e5f7eb13bf22ead1af00f6d96
Vivek Nair <vivek@nair.me>
2022-04-21 13:40:01 -0700

    MFKDF policy evaluate

---
+++ src/policy/evaluate.js
@@ -0,0 +1,36 @@
+/**
+ * @file MFKDF Policy Evaluation
+ * @copyright Multifactor 2022 All Rights Reserved
+ *
+ * @description
+ * Determine whether key can be derived from given factors
+ *
+ * @author Vivek Nair (https://nair.me) <vivek@nair.me>
+ */
+
+/**
+  * Evaluate a policy-based multi-factor derived key
+  *
+  * @example
+  * const result = await mfkdf.policy.evaluate( ... );
+  *
+  * @param {Object} policy - the key policy for the key being derived
+  * @param {Array.<string>} factors - array of factor ids used to derive this key
+  * @returns {boolean} Whether the key can be derived with given factor ids.
+  * @author Vivek Nair (https://nair.me) <vivek@nair.me>
+  * @since 0.16.0
+  * @memberOf policy
+  */
+function evaluate (policy, factors) {
+  const threshold = policy.threshold;
+  var actual = 0;
+  for (const factor of policy.factors) {
+    if (factor.type === 'stack') {
+      if (evaluate(factor.params, factors)) actual++;
+    } else {
+      if (factors.includes(factor.id)) actual++;
+    }
+  }
+  return (actual >= threshold);
+}
+module.exports.evaluate = evaluate
