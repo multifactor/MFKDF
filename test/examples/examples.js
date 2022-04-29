de1e95363c3e66c9c0737a1feb8e1779ef3456e2
Vivek Nair <vivek@nair.me>
2022-04-28 21:46:24 -0700

    new mfkdf homepage

--- test/examples/examples.js
+++ test/examples/examples.js
@@ -145,6 +145,26 @@ suite('examples', () => {
     key.toString('hex').should.equal('0394a2ede332c9a1')
   })
 
+  test('setup/derive fast', async () => {
+    // setup 16 byte 2-of-3-factor multi-factor derived key with a password, HOTP code, and UUID recovery code
+    const setup = await mfkdf.setup.key([
+      await mfkdf.setup.factors.password('password'),
+      await mfkdf.setup.factors.hotp({ secret: Buffer.from('hello world') }),
+      await mfkdf.setup.factors.uuid({ id: 'recovery', uuid: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d' })
+    ], { threshold: 2, size: 16, kdf: 'pbkdf2', pbkdf2rounds: 1 })
+
+    // derive key using 2 of the 3 factors
+    const derive = await mfkdf.derive.key(setup.policy, {
+      password: mfkdf.derive.factors.password('password'),
+      hotp: mfkdf.derive.factors.hotp(365287)
+    })
+
+    setup.key.toString('hex') // -> 34d20ced439ec2f871c96ca377f25771
+    derive.key.toString('hex') // -> 34d20ced439ec2f871c96ca377f25771
+
+    setup.key.toString('hex').should.equal(derive.key.toString('hex'))
+  })
+
   test('setup/derive', async () => {
     // setup 16 byte 2-of-3-factor multi-factor derived key with a password, HOTP code, and UUID recovery code
     const setup = await mfkdf.setup.key([
