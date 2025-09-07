1f7cce2f9cde8ef72b054bbd7cb991cab5494ab9
Vivek Nair <vivek@nair.me>
2025-09-07 07:11:41 -0700

    Deprecate kdfs

--- src/setup/kdf.js
+++ src/setup/kdf.js
@@ -41,10 +41,11 @@ const defaults = require('../defaults')
  * @author Vivek Nair (https://nair.me) <vivek@nair.me>
  * @since 0.7.0
  * @memberOf setup
+ * @deprecated
  */
 function kdf (options) {
   options = Object.assign(Object.assign({}, defaults.kdf), options)
-  if (typeof options.kdf !== 'string') throw new TypeError('kdf must be a string')
+  if (typeof options.kdf !== 'string') { throw new TypeError('kdf must be a string') }
   const config = {
     type: options.kdf,
     params: {}
@@ -52,56 +53,70 @@ function kdf (options) {
 
   if (options.kdf === 'hkdf') {
     // hdkf digest
-    if (typeof options.hkdfdigest !== 'string') throw new TypeError('hkdfdigest must be a string')
-    if (!['sha1', 'sha256', 'sha384', 'sha512'].includes(options.hkdfdigest)) throw new RangeError('hkdfdigest must be one of sha1, sha256, sha384, or sha512')
+    if (typeof options.hkdfdigest !== 'string') { throw new TypeError('hkdfdigest must be a string') }
+    if (!['sha1', 'sha256', 'sha384', 'sha512'].includes(options.hkdfdigest)) {
+      throw new RangeError(
+        'hkdfdigest must be one of sha1, sha256, sha384, or sha512'
+      )
+    }
     config.params.digest = options.hkdfdigest
   } else if (options.kdf === 'pbkdf2') {
     // pbkdf2 rounds
-    if (!(Number.isInteger(options.pbkdf2rounds))) throw new TypeError('pbkdf2rounds must be an integer')
-    if (!(options.pbkdf2rounds > 0)) throw new RangeError('pbkdf2rounds must be positive')
+    if (!Number.isInteger(options.pbkdf2rounds)) { throw new TypeError('pbkdf2rounds must be an integer') }
+    if (!(options.pbkdf2rounds > 0)) { throw new RangeError('pbkdf2rounds must be positive') }
     config.params.rounds = options.pbkdf2rounds
 
     // pbkdf2 digest
-    if (typeof options.pbkdf2digest !== 'string') throw new TypeError('pbkdf2digest must be a string')
-    if (!['sha1', 'sha256', 'sha384', 'sha512'].includes(options.pbkdf2digest)) throw new RangeError('pbkdf2digest must be one of sha1, sha256, sha384, or sha512')
+    if (typeof options.pbkdf2digest !== 'string') { throw new TypeError('pbkdf2digest must be a string') }
+    if (!['sha1', 'sha256', 'sha384', 'sha512'].includes(options.pbkdf2digest)) {
+      throw new RangeError(
+        'pbkdf2digest must be one of sha1, sha256, sha384, or sha512'
+      )
+    }
     config.params.digest = options.pbkdf2digest
   } else if (options.kdf === 'bcrypt') {
     // bcrypt rounds
-    if (!(Number.isInteger(options.bcryptrounds))) throw new TypeError('bcryptrounds must be an integer')
-    if (!(options.bcryptrounds > 0)) throw new RangeError('bcryptrounds must be positive')
+    if (!Number.isInteger(options.bcryptrounds)) { throw new TypeError('bcryptrounds must be an integer') }
+    if (!(options.bcryptrounds > 0)) { throw new RangeError('bcryptrounds must be positive') }
     config.params.rounds = options.bcryptrounds
   } else if (options.kdf === 'scrypt') {
     // scrypt rounds
-    if (!(Number.isInteger(options.scryptcost))) throw new TypeError('scryptcost must be a positive integer')
-    if (!(options.scryptcost > 0)) throw new RangeError('scryptcost must be positive')
+    if (!Number.isInteger(options.scryptcost)) { throw new TypeError('scryptcost must be a positive integer') }
+    if (!(options.scryptcost > 0)) { throw new RangeError('scryptcost must be positive') }
     config.params.rounds = options.scryptcost
 
     // scrypt block size
-    if (!(Number.isInteger(options.scryptblocksize))) throw new TypeError('scryptblocksize must be an integer')
-    if (!(options.scryptblocksize > 0)) throw new RangeError('scryptblocksize must be positive')
+    if (!Number.isInteger(options.scryptblocksize)) { throw new TypeError('scryptblocksize must be an integer') }
+    if (!(options.scryptblocksize > 0)) { throw new RangeError('scryptblocksize must be positive') }
     config.params.blocksize = options.scryptblocksize
 
     // scrypt parallelism
-    if (!(Number.isInteger(options.scryptparallelism))) throw new TypeError('scryptparallelism must be an integer')
-    if (!(options.scryptparallelism > 0)) throw new RangeError('scryptparallelism must be positive')
+    if (!Number.isInteger(options.scryptparallelism)) { throw new TypeError('scryptparallelism must be an integer') }
+    if (!(options.scryptparallelism > 0)) { throw new RangeError('scryptparallelism must be positive') }
     config.params.parallelism = options.scryptparallelism
-  } else if (options.kdf === 'argon2i' || options.kdf === 'argon2d' || options.kdf === 'argon2id') {
+  } else if (
+    options.kdf === 'argon2i' ||
+    options.kdf === 'argon2d' ||
+    options.kdf === 'argon2id'
+  ) {
     // argon2 rounds
-    if (!(Number.isInteger(options.argon2time))) throw new TypeError('argon2time must be an integer')
-    if (!(options.argon2time > 0)) throw new RangeError('argon2time must be positive')
+    if (!Number.isInteger(options.argon2time)) { throw new TypeError('argon2time must be an integer') }
+    if (!(options.argon2time > 0)) { throw new RangeError('argon2time must be positive') }
     config.params.rounds = options.argon2time
 
     // argon2 memory
-    if (!(Number.isInteger(options.argon2mem))) throw new TypeError('argon2mem must be an integer')
-    if (!(options.argon2mem > 0)) throw new RangeError('argon2mem must be positive')
+    if (!Number.isInteger(options.argon2mem)) { throw new TypeError('argon2mem must be an integer') }
+    if (!(options.argon2mem > 0)) { throw new RangeError('argon2mem must be positive') }
     config.params.memory = options.argon2mem
 
     // argon2 parallelism
-    if (!(Number.isInteger(options.argon2parallelism))) throw new TypeError('argon2parallelism must be an integer')
-    if (!(options.argon2parallelism > 0)) throw new RangeError('argon2parallelism must be positive')
+    if (!Number.isInteger(options.argon2parallelism)) { throw new TypeError('argon2parallelism must be an integer') }
+    if (!(options.argon2parallelism > 0)) { throw new RangeError('argon2parallelism must be positive') }
     config.params.parallelism = options.argon2parallelism
   } else {
-    throw new RangeError('kdf must be one of pbkdf2, bcrypt, scrypt, argon2i, argon2d, or argon2id')
+    throw new RangeError(
+      'kdf must be one of pbkdf2, bcrypt, scrypt, argon2i, argon2d, or argon2id'
+    )
   }
   return config
 }
