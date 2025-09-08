864a8d5aeb5be17d2f4deacb9109bf7e8e0a0ae9
Vivek Nair <vivek@nair.me>
2025-09-07 17:44:57 -0700

    Add tests

--- test/factors/hmacsha1.js
+++ test/factors/hmacsha1.js
@@ -73,6 +73,34 @@ suite('factors/hmacsha1', () => {
       )
   })
 
+  test('wrong-salt', async () => {
+    const setup =
+      '{"$schema":"https://mfkdf.com/schema/v2.0.0/policy.json","$id":"8b6874f7-5dbb-4196-bc89-347cd6b02dc6","size":32,"threshold":1,"salt":"nhh2/3AwOf2r2n7uRONoM697IjEKsHfAeyo8NxF1G94=","kdf":{"type":"argon2id","params":{"rounds":2,"memory":24576,"parallelism":1}},"factors":[{"id":"hmacsha1","type":"hmacsha1","pad":"PsS1B6fPovsuMfKZinw6hn0kTw1VEpoM8jRFR/8SyT0=","salt":"PlS1B6fPovsuMfKZinw6hn0kTw1VEpoM8jRFR/8SyT0=","params":{"challenge":"5a5f71c3a584b797d3c8f7d0f59653a2234781b06f2540df42946aa380f634a3430e6aad294e392543cc4ecd3da039bfa8041b179d14afd360a104e3354f01dd","pad":"f8d33204be0436f2629d5173e9bd3fc1953cf982"}}]}'
+
+    const derive = await mfkdf.derive.key(JSON.parse(setup), {
+      hmacsha1: mfkdf.derive.factors.hmacsha1(
+        Buffer.from('0f09b8c89bfefdbc4909432685358eca79912b6a', 'hex')
+      )
+    })
+
+    const setup2 =
+      '{"$schema":"https://mfkdf.com/schema/v2.0.0/policy.json","$id":"8b6874f7-5dbb-4196-bc89-347cd6b02dc6","size":32,"threshold":1,"salt":"nhh2/3AwOf2r2n7uRONoM697IjEKsHfAeyo8NxF1G94=","kdf":{"type":"argon2id","params":{"rounds":2,"memory":24576,"parallelism":1}},"factors":[{"id":"hmacsha1","type":"hmacsha1","pad":"PsS1B6fPovsuMfKZinw6hn0kTw1VEpoM8jRFR/8SyT0=","salt":"PsS1B6fPovsuMfKZinw6hn0kTw1VEpoM8jRFR/8SyT0=","params":{"challenge":"5a5f71c3a584b797d3c8f7d0f59653a2234781b06f2540df42946aa380f634a3430e6aad294e392543cc4ecd3da039bfa8041b179d14afd360a104e3354f01dd","pad":"f8d33204be0436f2629d5173e9bd3fc1953cf982"}}]}'
+
+    const derive2 = await mfkdf.derive.key(JSON.parse(setup2), {
+      hmacsha1: mfkdf.derive.factors.hmacsha1(
+        Buffer.from('0f09b8c89bfefdbc4909432685358eca79912b6a', 'hex')
+      )
+    })
+
+    derive.key
+      .toString('hex')
+      .should.not.equal(
+        '64393334663330303639356134323533393937376561636234363262373662316562616564393838663531393337303563666432633265373130636663666230'
+      )
+
+    derive.key.toString('hex').should.not.equal(derive2.key.toString('hex'))
+  })
+
   suite('errors', async () => {
     test('id/type', async () => {
       mfkdf.setup.factors
