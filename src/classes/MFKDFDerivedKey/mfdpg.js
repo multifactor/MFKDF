913dbb4d58a4b5f981136b269a915cfe5d1bd4a1
Vivek Nair <vivek@nair.me>
2025-09-11 21:27:16 -0700

    mfdpg test

--- src/classes/MFKDFDerivedKey/mfdpg.js
+++ src/classes/MFKDFDerivedKey/mfdpg.js
@@ -8,13 +8,30 @@
  * @author Vivek Nair (https://nair.me) <vivek@nair.me>
  */
 
-const RandExp = require('randexp')
-const rand = require('random-seed')
+const RandExp = require("randexp");
+const rand = require("random-seed");
 
 /**
  * Generate a policy-compliant password for a given purpose.
  *
  * @example
+ * const setup = await mfkdf.setup.key([
+ *   await mfkdf.setup.factors.password('password1', {
+ *     id: 'password1'
+ *   })
+ * ])
+ * const password = setup.derivePassword(
+ *   'example.com',
+ *   'salt',
+ *   /[a-zA-Z]{6,10}/
+ * )
+ *
+ * const password2 = setup.derivePassword(
+ *   'example.com',
+ *   'salt',
+ *   /[a-zA-Z]{6,10}/
+ * )
+ * password.should.equal(password2)
  *
  * @param {string} purpose - Unique purpose value for this password
  * @param {string} salt - Unique salt value for this salt
@@ -24,11 +41,11 @@ const rand = require('random-seed')
  * @since 2.0.0
  * @memberOf MFKDFDerivedKey
  */
-function derivePassword (purpose, salt, regex) {
-  const passwordKey = this.getSubkey(purpose, salt)
-  const dfa = new RandExp(regex)
-  const rng = rand.create(passwordKey.toString('hex'))
-  dfa.randInt = rng.intBetween
-  return dfa.gen()
+function derivePassword(purpose, salt, regex) {
+  const passwordKey = this.getSubkey(purpose, salt);
+  const dfa = new RandExp(regex);
+  const rng = rand.create(passwordKey.toString("hex"));
+  dfa.randInt = rng.intBetween;
+  return dfa.gen();
 }
-module.exports.derivePassword = derivePassword
+module.exports.derivePassword = derivePassword;
