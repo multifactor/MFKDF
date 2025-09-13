cb072a29f7c8123126ba0da406bcd0503679bff5
Vivek Nair <vivek@nair.me>
2025-09-13 12:05:47 -0700

    update .gitignore

--- site/old/docs.js
+++
@@ -1,3 +0,0 @@
-document.body.innerHTML = `
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/zephyr/bootstrap.min.css" integrity="sha512-gmJFuKTEQw6aHC2cWdrmdEHYw/M/8+bfsKmsJCcQ5hoI7WoOtkQ9hs7P0fqpBGRHHqBBJMibeCfhmk8So5qYUw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-` + document.body.innerHTML;
