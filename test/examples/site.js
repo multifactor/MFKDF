2717f46609d19d05eea2b496d65454a850fbb6f7
Vivek Nair <vivek@nair.me>
2025-09-15 12:36:40 -0700

    Fix tests

--- test/examples/site.js
+++ test/examples/site.js
@@ -1,99 +1,102 @@
 /* eslint no-unused-expressions: "off" */
-const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-chai.should();
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+chai.should()
 
-const mfkdf = require("../../src");
-const { suite, test } = require("mocha");
+const mfkdf = require('../../src')
+const { suite, test } = require('mocha')
 // const crypto = require('crypto')
 
-suite("site", () => {
-  test("Go beyond passwords", async () => {
+suite('site', () => {
+  test('Go beyond passwords', async () => {
     const keyPolicy = JSON.stringify(
       (
         await mfkdf.setup.key([
-          await mfkdf.setup.factors.password("Tr0ub4dour"),
+          await mfkdf.setup.factors.password('Tr0ub4dour'),
           await mfkdf.setup.factors.hotp({
-            secret: Buffer.from("abcdefghijklmnopqrst"),
+            secret: Buffer.from('abcdefghijklmnopqrst')
           }),
           await mfkdf.setup.factors.uuid({
-            id: "recovery",
-            uuid: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-          }),
+            id: 'recovery',
+            uuid: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+          })
         ])
       ).policy
-    );
+    )
 
     const derivedKey = await mfkdf.derive.key(JSON.parse(keyPolicy), {
-      password: mfkdf.derive.factors.password("Tr0ub4dour"),
+      password: mfkdf.derive.factors.password('Tr0ub4dour'),
       hotp: mfkdf.derive.factors.hotp(241063),
       recovery: mfkdf.derive.factors.uuid(
-        "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d"
-      ),
-    });
+        '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+      )
+    })
 
-    derivedKey.should.be.a("object");
-  });
+    derivedKey.should.be.a('object')
+  })
 
-  test("Increased key entropy", async () => {
+  test('Increased key entropy', async () => {
     Math.floor(
       (
         await mfkdf.setup.key([
-          await mfkdf.setup.factors.password("Tr0ub4dour"),
+          await mfkdf.setup.factors.password('Tr0ub4dour')
         ])
       ).entropyBits.real
-    ).should.equal(16);
+    ).should.equal(16)
 
     Math.floor(
       (
         await mfkdf.setup.key([
-          await mfkdf.setup.factors.password("Tr0ub4dour"),
+          await mfkdf.setup.factors.password('Tr0ub4dour'),
           await mfkdf.setup.factors.hotp(),
-          await mfkdf.setup.factors.hmacsha1(),
+          await mfkdf.setup.factors.hmacsha1()
         ])
       ).entropyBits.real
-    ).should.equal(196);
-  });
+    ).should.equal(196)
+  })
 
-  test("Enforce advanced policies", async () => {
+  test('Enforce advanced policies', async () => {
     const policyBasedKey = await mfkdf.policy.setup(
       await mfkdf.policy.or(
-        await mfkdf.setup.factors.uuid({ id: "recoveryCode" }),
+        await mfkdf.setup.factors.uuid({ id: 'recoveryCode' }),
         await mfkdf.policy.and(
-          await mfkdf.setup.factors.password("Tr0ub4dour"),
+          await mfkdf.setup.factors.password('Tr0ub4dour'),
           await mfkdf.setup.factors.totp()
         )
       )
-    );
-    policyBasedKey.should.be.a("object");
-  });
+    )
+    policyBasedKey.should.be.a('object')
+  })
 
-  test("Self-service factor recovery", async () => {
+  test('Self-service factor recovery', async () => {
     const keyPolicy = JSON.stringify(
       (
-        await mfkdf.setup.key([
-          await mfkdf.setup.factors.password("password"),
-          await mfkdf.setup.factors.hotp({
-            secret: Buffer.from("abcdefghijklmnopqrst"),
-          }),
-          await mfkdf.setup.factors.uuid({
-            id: "recoveryCode",
-            uuid: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-          }),
-        ])
+        await mfkdf.setup.key(
+          [
+            await mfkdf.setup.factors.password('password'),
+            await mfkdf.setup.factors.hotp({
+              secret: Buffer.from('abcdefghijklmnopqrst')
+            }),
+            await mfkdf.setup.factors.uuid({
+              id: 'recoveryCode',
+              uuid: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+            })
+          ],
+          { threshold: 2 }
+        )
       ).policy
-    );
+    )
 
     const key = await mfkdf.derive.key(JSON.parse(keyPolicy), {
       hotp: mfkdf.derive.factors.hotp(241063),
       recoveryCode: mfkdf.derive.factors.uuid(
-        "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d"
-      ),
-    });
+        '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+      )
+    })
 
     await key.recoverFactor(
-      await mfkdf.setup.factors.password("myNewPassword", { id: "password" })
-    ); // modify key to use new password factor
-  });
-});
+      await mfkdf.setup.factors.password('myNewPassword', { id: 'password' })
+    ) // modify key to use new password factor
+  })
+})
